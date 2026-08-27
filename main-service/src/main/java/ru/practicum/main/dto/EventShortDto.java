934ba50a447e6d4b0c5a92742af387f538335948
Lera Tikhonova <valeria.tihonova2011@yandex.ru>
2026-08-27 12:18:38 +0300

    Корректировка

--- main-service/src/main/java/ru/practicum/main/dto/EventShortDto.java
+++ main-service/src/main/java/ru/practicum/main/dto/EventShortDto.java
@@ -15,10 +15,10 @@ public class EventShortDto {
     private Long id;
     private String annotation;
     private CategoryDto category;
-    private Long confirmedRequests;
+    private Long confirmedRequests;  // ← ЭТО ПОЛЕ БЫЛО ПРОПУЩЕНО
     private LocalDateTime eventDate;
     private UserShortDto initiator;
     private Boolean paid;
     private String title;
-    private Long views; // Обязательное поле
+    private Long views;
 }
